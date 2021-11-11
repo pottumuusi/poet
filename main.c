6c4bc5d7e5fe45c024db85ffde482b71d30c3763
Aleksi Backman <arbiter@it.justworks.today>
2021-11-11 06:20:58 +0000

    More IO initialization and print 10x10 box

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <ncurses.h>
 
+#define ROW_ZERO 2
+#define COL_ZERO 5
+
 int main(void) {
 	initscr();
-	move(2, 5);
-	printw("Hello World!");
+	noecho(); // Do not echo read chars
+	cbreak(); // Read input contiguously
+
+	move(ROW_ZERO, COL_ZERO);
+	for (int i = 0; i < 10; i++) {
+		printw("..........");
+		move(ROW_ZERO + i, COL_ZERO);
+	}
 	refresh();
+
 	getch();
 	endwin();
 
