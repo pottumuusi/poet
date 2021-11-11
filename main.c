29b3819fa71bfc8bb91069596b5ead27297c7b6e
Aleksi Backman <arbiter@it.justworks.today>
2021-11-11 06:25:36 +0000

    Print terrain by using max values for rows and columns

--- main.c
+++ main.c
@@ -3,15 +3,20 @@
 #define ROW_ZERO 2
 #define COL_ZERO 5
 
+#define ROW_MAX 15
+#define COL_MAX 25
+
 int main(void) {
 	initscr();
 	noecho(); // Do not echo read chars
 	cbreak(); // Read input contiguously
 
 	move(ROW_ZERO, COL_ZERO);
-	for (int i = 0; i < 10; i++) {
-		printw("..........");
-		move(ROW_ZERO + i, COL_ZERO);
+	for (int i = 0; i < ROW_MAX; i++) {
+		for (int k = 0; k < COL_MAX; k++) {
+			move(ROW_ZERO + i, COL_ZERO + k);
+			addch('.');
+		}
 	}
 	refresh();
 
