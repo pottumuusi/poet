d9b98081715066e891ec52744bedd97d39c8b435
Aleksi Backman <arbiter@it.justworks.today>
2021-11-11 08:02:45 +0000

    Add movement for player

--- main.c
+++ main.c
@@ -3,10 +3,14 @@
 #define ROW_ZERO 2
 #define COL_ZERO 5
 
+#define ROW_DEBUG_ZERO 35
+#define COL_DEBUG_ZERO 5
+
 #define ROW_MAX 15
 #define COL_MAX 25
 
 #define ALL_ACTORS_SIZE 32
+#define ALL_ACTORS_PLAYER_INDEX 0
 #define ICON_PLAYER '@'
 
 #define BUTTON_QUIT 'q'
@@ -31,7 +35,7 @@ void draw_layer_terrain(void)
 	}
 }
 
-void draw_layer_actors(const struct actor ** const all_actors)
+void draw_layer_actors(struct actor ** const all_actors)
 {
 #if DEBUG_PRINT
 	printw("all_actors[0] is: %08x\n", all_actors[0]);
@@ -46,11 +50,75 @@ void draw_layer_actors(const struct actor ** const all_actors)
 	}
 }
 
-void draw(const struct actor ** const all_actors)
+void draw(struct actor ** const all_actors)
 {
 	draw_layer_terrain();
 	draw_layer_actors(all_actors);
 	refresh();
+}
+
+int is_position_button(const int pressed_key)
+{
+	if (KEY_DOWN == pressed_key) {
+		return 1;
+	}
+
+	if (KEY_UP == pressed_key) {
+		return 1;
+	}
+
+	if (KEY_LEFT == pressed_key) {
+		return 1;
+	}
+
+	if (KEY_RIGHT == pressed_key) {
+		return 1;
+	}
+
+	if (' ' == pressed_key) {
+		return 1;
+	}
+
+	return 0;
+}
+
+void update_position(
+		const int pressed_key,
+		struct actor* const actor)
+{
+	// TODO collision detection
+
+	if (KEY_DOWN == pressed_key) {
+		actor->row = actor->row + 1;
+	}
+
+	if (KEY_UP == pressed_key) {
+		actor->row = actor->row - 1;
+	}
+
+	if (KEY_LEFT == pressed_key) {
+		actor->col = actor->col - 1;
+	}
+
+	if (KEY_RIGHT == pressed_key) {
+		actor->col = actor->col + 1;
+	}
+}
+
+void update_player(const int pressed_key, struct actor* const player_actor)
+{
+	if (is_position_button(pressed_key)) {
+		update_position(pressed_key, player_actor);
+	}
+}
+
+void update(
+		const int pressed_key,
+		struct actor ** const all_actors)
+{
+	update_player(pressed_key, all_actors[ALL_ACTORS_PLAYER_INDEX]);
+	// Player and UI related updates dependent on user input.
+	// Enemy updates independent of user input.
 }
 
 void initialize_io(void)
@@ -64,7 +132,7 @@ void initialize_io(void)
 int main(void) {
 	int pressed_key = 0;
 
-	const struct actor* all_actors[ALL_ACTORS_SIZE] = {0};
+	struct actor* all_actors[ALL_ACTORS_SIZE] = {0};
 	struct actor player = {
 		.row = ROW_ZERO + 2,
 		.col = COL_ZERO + 2,
@@ -72,13 +140,14 @@ int main(void) {
 		.name = "wizard",
 	};
 
-	all_actors[0] = &player;
+	all_actors[ALL_ACTORS_PLAYER_INDEX] = &player;
 
 	initialize_io();
 
 	while (BUTTON_QUIT != pressed_key) {
 		draw(all_actors);
 		pressed_key = getch();
+		update(pressed_key, all_actors);
 	}
 
 	endwin();
