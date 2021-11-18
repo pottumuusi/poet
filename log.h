0e37881978b6de54db75c6acf61bfed4c9fd6178
Aleksi Backman <arbiter0xf@protonmail.com>
2021-11-18 12:49:37 +0200

    Write log entries to file at the time of LOG_COMMON call
    Log entries were written on program exit.

--- log.h
+++ log.h
@@ -14,14 +14,15 @@ extern char g_log_buffer[LOG_STR_SIZE];
 #define LOG_WARNING(new_log, ...) LOG_COMMON("[WARN] " , new_log, __VA_ARGS__)
 #define LOG_ERROR(new_log, ...) LOG_COMMON("[ERR ] " , new_log, __VA_ARGS__)
 
-#define LOG_COMMON(priority, new_log, ...)	\
-	bzero(g_log_buffer, LOG_STR_SIZE);	\
-	strcat(g_log_buffer, priority);		\
-	strcat(g_log_buffer, new_log);		\
-	g_log_buffer[LOG_STR_SIZE - 1] = '\0';	\
-	assert(g_log_handle);			\
-	assert(g_log_buffer);			\
-	fprintf(g_log_handle, g_log_buffer, __VA_ARGS__);
+#define LOG_COMMON(priority, new_log, ...)			\
+	bzero(g_log_buffer, LOG_STR_SIZE);			\
+	strcat(g_log_buffer, priority);				\
+	strcat(g_log_buffer, new_log);				\
+	g_log_buffer[LOG_STR_SIZE - 1] = '\0';			\
+	assert(g_log_handle);					\
+	assert(g_log_buffer);					\
+	fprintf(g_log_handle, g_log_buffer, __VA_ARGS__);	\
+	fflush(g_log_handle);
 
 void initialize_logging(void);
 void teardown_logging(void);
