c2f5ce375213145497a7f7ffd199829f724c820c
Aleksi Backman <arbiter0xf@protonmail.com>
2021-11-27 12:01:01 +0200

    Variable for dependencies install directory

--- scripts/install_criterion.sh
+++ scripts/install_criterion.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-mkdir deps_install
-pushd deps_install
+readonly deps_install_dir="deps_install"
+
+mkdir ${deps_install_dir}
+pushd ${deps_install_dir}
+
 wget https://github.com/Snaipe/Criterion/releases/download/v2.3.3/criterion-v2.3.3-linux-x86_64.tar.bz2
 tar -xjvf criterion-v2.3.3-linux-x86_64.tar.bz2
 cd criterion-v2.3.3
@@ -9,5 +12,6 @@ cd include
 sudo cp -rv criterion /usr/include/
 cd ../
 sudo cp -v lib/libcriterion* /usr/lib64/
+
 popd
-rm -r deps_install
+rm -r ${deps_install_dir}
